b21f6d5930febc4b83a2ef95039c1378144f451c
Sahil964-star <sahilvedan234@gmail.com>
2026-06-07 17:14:19 +0530

    Added menu system

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def show_menu():
+    print("\n------ Expense Tracker ------")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Filter By Category")
+    print("4. Exit")
+
 def main():
-    print("Personal expense tracker")
+    while True:
+        show_menu()
+
+        choice=input("Enter choice: ")
+
+        if choice=="4":
+            print("Exiting")
+            break
+
 main()
