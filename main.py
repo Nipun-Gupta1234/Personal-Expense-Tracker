6bb53583c585b39657a144e16741d49b5be505f4
Sahil964-star <sahilvedan234@gmail.com>
2026-06-08 18:19:27 +0530

    Implemented filter_expenses feature

--- main.py
+++ main.py
@@ -41,3 +41,50 @@ def view_expenses():
             print(f"Total expense={total}")
     except FileNotFoundError:
         print("No expenses found")
+
+def filter_expenses():
+    wanted_category=input("Enter category: ")
+
+    try:
+        with open("expenses.txt","r") as file:
+            subtotal=0
+            found=False
+
+            print("\nDate\t\tAmount\tNote")
+            print("-" * 60)
+
+            for line in file:
+                date, category, amount, note=line.strip().split(",")
+                if category.lower()==wanted_category.lower():
+                    print(f"{date}\t{amount}\t{note}")
+                    subtotal+=float(amount)
+                    found=True
+
+            if found:
+                print("-"*60)
+                print(f"Subtotal={subtotal}")
+
+            else:
+                print("No expenses found in this category")
+
+    except FileNotFoundError:
+        print("No expenses found")
+
+    def main():
+        while True:
+            show_menu()
+            choice=input("Enter choice: ")
+
+            if choice=="1":
+                add_expense()
+            elif choice=="2":
+                view_expenses()
+            elif choice=="3":
+                filter_expenses()
+            elif choice=="4":
+                print("Exiting...")
+                break
+            else:
+                print("Invalid choice")
+
+    main()
